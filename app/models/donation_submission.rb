e6f8c747f1a2ae561fed36bc30cb578560d6403d
Anuja Joshi <joshi_anuja@rocketmail.com>
2015-05-23 13:10:13 +0530

    Added  model DonationSubmission

---
+++ app/models/donation_submission.rb
@@ -0,0 +1,11 @@
+class DonationSubmission
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :cash_amount_pending, type: Integer
+  field :cheque_amount_pending, type: Integer
+  field :amount_collected_by_cash, type: Integer
+  field :amount_collected_by_cheque, type: Integer
+  
+  #add relationship with user model
+end
