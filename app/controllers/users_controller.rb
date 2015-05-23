1d0a2fb8d134c9cc63c146a5fe9bd197040e039c
atulonruby <atuljha86@gmail.com>
2015-05-23 16:41:14 +0530

    super admin can added users now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,22 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      render 'index'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      [:email,:password,:password_confirmation,:role,:contact_number,:name,:company_name])
   end
 end
