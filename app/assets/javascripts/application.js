18c5f7ac06517dc5d12ad99ecd5839fbc0e4fe93
Amol <amoludage@joshsoftware.com>
2015-05-23 15:02:32 +0530

    add datatable for user index page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require dataTables/jquery.dataTables
+//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
+//= require dataTables/jquery.dataTables
 //= require turbolinks
 //= require_tree .
+
