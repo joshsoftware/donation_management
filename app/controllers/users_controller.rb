4efcbe4bbc3f05aa19fe07ab5b8d6ed2a8a694db
atulonruby <atuljha86@gmail.com>
2015-05-23 18:54:43 +0530

    email is sent to the invited user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      @user.invite!(current_user)
       render 'index'
     else
       render 'new'
