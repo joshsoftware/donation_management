796b2bd4e86cbab62420cc834eb24929f9a10cc4
Shweta <shweta@joshsoftware.com>
2015-05-23 18:47:42 +0530

    unicorn conf [#12]

---
+++ config/unicorn.rb
@@ -0,0 +1,10 @@
+# Setting the number of worker processes
+worker_processes 2
+#
+# Time after which a worker is restarted if unresponsive
+timeout 120         
+#
+# Load the application before forking workers.
+# set to true if you use NewRelic (which you should) or you won't see any data
+#preload_app true   
+#
