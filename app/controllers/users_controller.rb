32ef012749752b9e3e446240a601ffffccc977e3
nitinzd <Nitin@Nitin-PC.(none)>
2015-05-23 18:28:05 +0530

    Modified user/edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,19 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    respond_to do |format|
+      if @user.update_attributes()
+        format.html {redirect_to users_path, notice: "User has been updated successfully"}
+      else
+        format.html {render action: "edit"}
+      end
+    end
+
   end
 
   def new
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def can_edit(role)
+    if role == "Super Admin" || role == "Admin"
+      true
+    else
+      false
+    end
+  end
+
 end
