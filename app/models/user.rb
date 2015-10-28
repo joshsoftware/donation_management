a203ab4d9a81932c2659e4ada8f5b49f64e415e1
Shweta <shweta@joshsoftware.com>
2015-10-28 20:15:13 +0530

    Commenting out devise registerable.
    - It was added for change password link but was giving some problem with
    invitation(need to check this out)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   devise :invitable, :database_authenticatable,
-    :recoverable, :rememberable, :trackable, :validatable, :registerable
+    :recoverable, :rememberable, :trackable, :validatable#, :registerable
 
   Roles = ['Super Admin', 'Admin', 'Coordinator']
   
